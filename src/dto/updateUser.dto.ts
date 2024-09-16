486d3e7849fec4c7da743e383c497d755fd0cbf7
Nikita Loshkarev <nilo@Nikitas-MacBook-Pro.local>
2024-09-16 13:26:47 +0400

    update .env.examle

--- src/dto/updateUser.dto.ts
+++ src/dto/updateUser.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from "@nestjs/mapped-types";
-import { CreateUserDto } from "./user.dto";
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateUserDto } from './user.dto';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {}
