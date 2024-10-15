45b78ae88fc27a6c5d4ba624caf0f0ea90437f46
Nikita Loshkarev <nilo@Nikitas-MacBook-Pro.local>
2024-10-15 13:34:32 +0400

    add swagger to project

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,22 @@ import { AppModule } from './modules/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from './exceptions/allException.filter';
 import { HttpExceptionFilter } from './exceptions/http-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Test API')
+    .setDescription('The test API description')
+    .setVersion('1.0')
+    .addServer('http://localhost:3000/', 'Local environment')
+    .addTag('test')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   app.useGlobalPipes(new ValidationPipe());
   const httpAdapter = app.get(HttpAdapterHost);
   app.useGlobalFilters(
